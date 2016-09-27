87bf34e06f9b57b006aecf4269dc19c44678f857
Xon <tazipper1@hotmail.com>
2016-09-27 08:07:55 +0800

    freegeoip is often slow and unreliable, use it last

--- upload/library/TPUDetectSpamReg/IPCountry.php
+++ upload/library/TPUDetectSpamReg/IPCountry.php
@@ -17,13 +17,6 @@ class TPUDetectSpamReg_IPCountry
 
 		try
 		{
-			$country=json_decode(file_get_contents('https://freegeoip.net/json/'.$ip));
-			if (isset($country) && isset($country->country_code) && $country->country_code!='')
-				return $country->country_code;
-		} catch(Exception $e) {}
-
-		try
-		{
 			$country=json_decode(file_get_contents('http://ip-api.com/json/'.$ip));
 			if (isset($country) && isset($country->countryCode) && $country->countryCode!='')
 				return $country->countryCode;
@@ -34,6 +27,13 @@ class TPUDetectSpamReg_IPCountry
 			$country=trim(file_get_contents("http://api.hostip.info/country.php?ip=".$ip));
 			if ($country!='XX')
 				return $country;
+		} catch(Exception $e) {}
+
+		try
+		{
+			$country=json_decode(file_get_contents('https://freegeoip.net/json/'.$ip));
+			if (isset($country) && isset($country->country_code) && $country->country_code!='')
+				return $country->country_code;
 		} catch(Exception $e) {}
 
 		return 'XX';
