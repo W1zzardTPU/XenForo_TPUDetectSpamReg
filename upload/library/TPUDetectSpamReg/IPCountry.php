8cb43fdd0f9dd52f2deaa92c06fa5416af47d13b
Xon <tazipper1@hotmail.com>
2017-02-14 19:35:29 +0800

    Remove dead site

--- upload/library/TPUDetectSpamReg/IPCountry.php
+++ upload/library/TPUDetectSpamReg/IPCountry.php
@@ -24,13 +24,6 @@ class TPUDetectSpamReg_IPCountry
 
 		try
 		{
-			$country=trim(file_get_contents("http://api.hostip.info/country.php?ip=".$ip));
-			if ($country!='XX')
-				return $country;
-		} catch(Exception $e) {}
-
-		try
-		{
 			$country=json_decode(file_get_contents('https://freegeoip.net/json/'.$ip));
 			if (isset($country) && isset($country->country_code) && $country->country_code!='')
 				return $country->country_code;
